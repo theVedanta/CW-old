7c2a7925d9496c776cac2dc54e22f725c0689feb
Vedanta Somnathe <vedusom142005@gmail.com>
2021-02-05 18:36:58 +0530

    change it is

--- js/contacts.js
+++ js/contacts.js
@@ -1,25 +1,14 @@
-// const emailTxt = document.querySelector("#email");
-// const copyBtn = document.querySelector("#copyBtn");
+const copyBtns = document.querySelectorAll(".mailBtn");
 
-// copyBtn.addEventListener(`click`, () => {
-//   emailTxt.select();
-//   emailTxt.setSelectionRange(0, 99999);
-//   document.execCommand("copy");
-//   alert("Copied the text: " + emailTxt.value);
-// });
-
-const divEmail = document.getElementById("divEmail");
-const copyBtn2 = document.getElementById("copyBtn2");
-
-function copy() {
-    const range = document.createRange();
-    window.getSelection().removeAllRanges();
-    range.selectNode(divEmail);
-    window.getSelection().addRange(range);
-    document.execCommand("copy");
-    window.getSelection().removeAllRanges();
+for (let copyBtn of copyBtns) {
+    copyBtn.addEventListener("click", () => {
+        const range = document.createRange();
+        window.getSelection().removeAllRanges();
+        const card = copyBtn.parentElement;
+        const divEmail = card.querySelector(".mail");
+        range.selectNode(divEmail);
+        window.getSelection().addRange(range);
+        document.execCommand("copy");
+        window.getSelection().removeAllRanges();
+    });
 };
-
-copyBtn2.addEventListener("click", copy)
-
-
