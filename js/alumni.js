00e2732368abc7535de2544e56ef046923eaaf21
Vedanta Somnathe <vedusom142005@gmail.com>
2021-02-08 12:39:19 +0530

    alumni stuff

---
+++ js/alumni.js
@@ -0,0 +1,61 @@
+let arrs = document.querySelectorAll(".arr");
+
+for (let arr of arrs) {
+    arr.addEventListener("click", e => {
+        arr.classList.toggle("arrActive");
+        let card = arr.parentElement;
+        let write = card.querySelector(".write");
+        write.classList.toggle("writeActive");
+    });
+};
+
+// SEARCH
+let search = document.querySelector("#searchBar");
+let cards = document.querySelectorAll(".card");
+
+let titles = [];
+for (let card of cards) {
+    let h2 = card.querySelector("h2").textContent.toLowerCase();
+    let h3 = card.querySelector("h3").textContent.toLowerCase();
+    titles.push(h2);
+    titles.push(h3);
+};
+
+search.addEventListener("keyup", e => {
+    let usrInp = search.value.trim();
+    let matches = getMatches(usrInp);
+    if (usrInp == "") {
+        for (let card of cards) {
+            card.style.display = "flex";
+        };
+    } else {
+        for (let card of cards) {
+            card.style.display = "none";
+        };
+    };
+    for (let match of matches) {
+        for (let title of titles) {
+            if (match == title) {
+                for (let card of cards) {
+                    if (card.querySelector("h2").textContent.toLowerCase() == title) {
+                        card.style.display = "flex";
+                    } else if (card.querySelector("h3").textContent.toLowerCase() == title) {
+                        card.style.display = "flex";
+                    }
+                };
+            };
+        };
+    };
+});
+
+function getMatches(input) {
+    let matchList = [];
+
+    for (let i = 0; i < titles.length; i++) {
+        if (titles[i].toLowerCase().indexOf(input.toLowerCase()) != -1) {
+            matchList.push(titles[i]);
+        };
+    };
+
+    return matchList;
+};
