83c6f65a8fd05f35dc1e17230690eece260f1163
Vedanta Somnathe <vedusom142005@gmail.com>
2021-03-27 11:51:05 +0530

    load anim

--- js/blog.js
+++ js/blog.js
@@ -26,3 +26,78 @@ sorter.addEventListener("click", () => {
 
 // const arrow = document.querySelector('.arrow-wrap')
 // arrow.style.top = screenH
+
+// SEARCH
+function getMatches(input) {
+  let matchList = [];
+
+  for (let i = 0; i < titles.length; i++) {
+    if (titles[i].toLowerCase().indexOf(input.toLowerCase()) != -1) {
+      matchList.push(titles[i]);
+    }
+  }
+
+  return matchList;
+}
+
+let search = document.querySelector("#searchBar");
+let blogTitles = document.querySelectorAll(".unit h1");
+
+let titles = [];
+for (let title of blogTitles) {
+  let h2 = title.textContent.toLowerCase();
+  titles.push(h2);
+}
+
+let domTitles = document.querySelectorAll(".unit h1");
+
+search.addEventListener("keyup", () => {
+  document.querySelector(".items").style.display = "none";
+  let usrInp = search.value.trim();
+  let matches = getMatches(usrInp);
+  if (usrInp == "") {
+    for (let domTitle of domTitles) {
+      domTitle.parentElement.style.display = "none";
+    }
+    document.querySelector(".titles").style.display = "none";
+    document.querySelector(".items").style.display = "inline-block";
+  } else {
+    document.querySelector(".titles").style.display = "inline-block";
+    for (let match of matches) {
+      for (let title of titles) {
+        if (match == title) {
+          for (let domTitle of domTitles) {
+            if (domTitle.textContent.toLowerCase() == title) {
+              domTitle.parentElement.style.display = "block";
+            }
+          }
+        }
+      }
+    }
+  }
+});
+
+// SORTING
+let options = document.querySelectorAll(".option");
+let yrs = document.querySelectorAll(".year");
+
+for (let option of options) {
+  option.addEventListener("click", () => {
+    if (option.textContent == "All") {
+      for (let bog of document.querySelectorAll(".item")) {
+        bog.style.display = "inline-block";
+      }
+      option.style.display = "none";
+    } else {
+      for (let bog of document.querySelectorAll(".item")) {
+        bog.style.display = "none";
+      }
+      for (let yr of yrs) {
+        if (parseInt(yr.textContent) == parseInt(option.textContent)) {
+          yr.parentElement.style.display = "inline-block";
+        }
+      }
+      document.querySelector(".allSort").style.display = "block";
+    }
+  });
+}
--- js/mode.js
+++ js/mode.js
@@ -49,8 +49,21 @@ let body = document.body;
 let shapes = document.querySelectorAll(".transparent img");
 
 let loaded = () => {
-  loader.classList.add("loaded");
-  body.classList.remove("loading");
+  document.querySelector(".shap").classList.add("shapL");
+  document.querySelector(".lin").classList.add("linL");
+  setTimeout(() => {
+    document.querySelector(".loader h1").textContent = "C";
+  }, 600);
+  setTimeout(() => {
+    document.querySelector(".loader h1").textContent = "CW";
+  }, 800);
+  setTimeout(() => {
+    document.querySelector(".lin").classList.add("blink");
+  }, 1000);
+  setTimeout(() => {
+    body.classList.remove("loading");
+    loader.classList.add("loaded");
+  }, 1200);
 };
 
 let inital = () => {
@@ -75,10 +88,10 @@ let heroStuff = () => {
 };
 
 let load = () => {
-  loaded();
-  setTimeout(inital, 400);
-  setTimeout(heroStuff, 400);
-  setTimeout(theTrans, 402);
+  setTimeout(loaded, 1400);
+  setTimeout(inital, 1400);
+  setTimeout(heroStuff, 1400);
+  setTimeout(theTrans, 1402);
 };
 
 $(window).on("load", load);
